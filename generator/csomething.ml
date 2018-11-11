f1624aa903f23d052395cb1561775787c4e05178
Zixiong Liu <zl2683@columbia.edu>
2018-11-11 01:50:43 +0000

    remove generator

--- generator/csomething.ml
+++
@@ -1,8 +0,0 @@
-
-let in_channel = open_in "test.c"
-
-
-let _ =   
-   let lexbuf = Lexing.from_channel in_channel in
-    let ast = Parser.program Scanner.token lexbuf in
-    print_string (Ast.string_of_program ast)
--- generator/test.c
+++
@@ -1,9 +0,0 @@
-int main(){
-    int b;
-    bool foo;
-    float d;
-    int a;
-    a = 5;
-    d = 1.2;
-    return 0;
-}

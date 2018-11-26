7c6364033b5f16b0f64fbe7dd991ece90a187e9d
mshzhb <mshzhb@gmail.com>
2018-11-26 17:23:55 -0500

    fix matrix_lib error

--- stdlib/matrix_lib.c
+++ stdlib/matrix_lib.c
@@ -8,21 +8,21 @@
  struct matrix {
     size_t m, n;
     int type; // 0 = int, 1 = float, 2 = double, etc.
-    void *data; // the actual matrix entries on the heap 
- }
+    void* data; // the actual matrix entries on the heap 
+ };
  
  struct vector {
     size_t n;
     int type; // 0 = int, 1 = float, 2 = double, etc.
-    void *data; // the actual matrix entries on the heap 
- }
+    void* data; // the actual matrix entries on the heap 
+ };
 
-struct matrix;
-struct vector;
+typedef struct matrix Mat;
+typedef struct vector Vec;
 
 /* allocates an m*n matrix of type int on the heap */
-struct matrix *alloc_mat_int(size_t m, size_t n){
-    struct matrix * matrix_ptr = malloc(sizeof (struct matrix));
+Mat* alloc_mat_int(size_t m, size_t n){
+    Mat* matrix_ptr = malloc(sizeof (Mat));
     if(matrix_ptr == NULL){
     	exit(-1);
     }
@@ -34,12 +34,12 @@ struct matrix *alloc_mat_int(size_t m, size_t n){
     matrix_ptr->type = 0;
     matrix_ptr->m = m; 
     matrix_ptr->n = n;
-    return matrix_ptr
+    return matrix_ptr;
 }
 
 /* allocate matrix of floats */
-struct matirx *alloc_mat_float(size_t m, size_t n){
-    struct matrix * matrix_ptr = malloc(sizeof (struct matrix));
+Mat* alloc_mat_float(size_t m, size_t n){
+    Mat * matrix_ptr = malloc(sizeof (Mat));
     if(matrix_ptr == NULL){
     	exit(-1);
     }
@@ -51,16 +51,16 @@ struct matirx *alloc_mat_float(size_t m, size_t n){
     matrix_ptr->type = 0;
     matrix_ptr->m = m; 
     matrix_ptr->n = n;
-    return matrix_ptr
+    return matrix_ptr;
 }
 
 /* allocate an array of integers of size n */
 struct vector *alloc_vec_int(size_t n){
-	struct vector vector_ptr = malloc(sizeof(struct  vector));
+	struct vector* vector_ptr = malloc(sizeof(struct  vector));
 	if(vector_ptr == NULL){
 		exit(-1);
 	}
-	vector_ptr->data = malloc(n * sizeof(int))
+	vector_ptr->data = malloc(n * sizeof(int));
 	if(vector_ptr->data == NULL){
 		free(vector_ptr);
 		exit(-1);
@@ -71,11 +71,11 @@ struct vector *alloc_vec_int(size_t n){
 
 /* allocate an array of floats */
 struct vector *alloc_vec_float(size_t n){
-	struct vector vector_ptr = malloc(sizeof(struct  vector));
+	struct vector* vector_ptr = malloc(sizeof(struct  vector));
 	if(vector_ptr == NULL){
 		exit(-1);
 	}
-	vector_ptr->data = malloc(n * sizeof(float))
+	vector_ptr->data = malloc(n * sizeof(float));
 	if(vector_ptr->data == NULL){
 		free(vector_ptr);
 		exit(-1);
@@ -85,9 +85,9 @@ struct vector *alloc_vec_float(size_t n){
 }
 
 /* this should call whatever destructor necessary and call free */
-void free_matrix(struct matrix *m){
+void free_matrix(struct matrix* m){
     if(m != NULL){
-    	free(matrix->data);
+    	free(m->data);
     	free(m);
     }
     
@@ -102,41 +102,41 @@ void free_vector(struct vector *v){
 
 /* fill the matrix m with entries from one-dimensional array "data", row-major ordering */
 void fill_mat_int(struct matrix *m, int *data){
-    num_rows = m->m; 
-    num_columns = m->n;
+    int num_rows = m->m; 
+    int num_columns = m->n;
     int offset = 0;
     for(int i = 0; i < num_rows; i++){
     	for(int j = 0; j < num_columns; j++){
     		offset = i * num_columns + j;
-    		m->data[offset] = *(data + (i * j));
+    		((int*)(m->data))[offset] = *(data + (i * j));
     	}	
     }
     
 }
 
 void fill_mat_float(struct matrix *m, float *data){
-	num_rows = m->m; 
-    num_columns = m->n;
+    int num_rows = m->m; 
+    int num_columns = m->n;
     int offset = 0;
     for(int i = 0; i < num_rows; i++){
     	for(int j = 0; j < num_columns; j++){
     		offset = i * num_columns + j;
-    		m->data[offset] = *(data + (i * j));
+    		*(((float*)m->data) + offset) = *(data + (i * j));
     	}	
     }
     
 }
 
 void fill_vec_int(struct vector *v, int *data){
-	for(int i = 0; i < num_rows; i++){
-    	v->data[i] = *(data + i);
-    }
+	for(int i = 0; i < v->n; i++)
+    		*((int*)(v->data) + i) = *(data + i);
+    
 }
 
 void fill_vec_float(struct vector *v, float *data){
-	for(int i = 0; i < num_rows; i++){
-    	v->data[i] = *(data + i);
-    }
+	for(int i = 0; i < v->n; i++)
+    		*((float*)(v->data) + i) = *(data + i);
+
 }
 
 /* In case of index-out-of-bounds, the functions should
@@ -152,7 +152,7 @@ int *get_index_matrix_int(struct matrix *m, size_t i, size_t j){
 		write(2, str, strlen(str));
 		exit(-1);
 	}
-	int offset = i * num_columns + j;
+	int offset = i * m->n + j;
 	int * index_pointer; 
 	index_pointer = &m->data[offset];
 	return index_pointer;
@@ -165,114 +165,120 @@ float *get_index_matrix_float(struct matrix *m, size_t i, size_t j){
 		write(2, str, strlen(str));
 		exit(-1);
 	}
-	int offset = i * num_columns + j;
+	int offset = i * m->n + j;
 	int * index_pointer; 
 	index_pointer = &m->data[offset];
 	return index_pointer;
 }
 
 int *get_index_vec_int(struct vector *v, size_t i){
-	if(i > m->n){
+	if(i > v->n){
 		char str[] = "Index out of bounds";
 		write(2, str, strlen(str));
 		exit(-1);
 	}
 	int * index_pointer;
 	index_pointer = &v->data[i];
-	return index_pointer
+	return index_pointer;
 }
 
 float *get_index_vec_float(struct vector *v, size_t i){
-	if(i > m->n){
+	if(i > v->n){
 		char str[] = "Index out of bounds";
 		write(2, str, strlen(str));
 		exit(-1);
 	}
 	float * index_pointer;
 	index_pointer = &v->data[i];
-	return index_pointer
+	return index_pointer;
 }
 
 
 /* the following are linear algebra operations */
 
 void addition_mat_int(struct matrix *dest, struct matrix *arg1, struct matrix *arg2){
-	num_rows = arg1->m; 
-    num_columns = arg1->n;
+    int num_rows = arg1->m; 
+    int num_columns = arg1->n;
     int offset = 0;
     for(int i = 0; i < num_rows; i++){
     	for(int j = 0; j < num_columns; j++){
     		offset = i * num_columns + j;
-    		dest->data[offset] = arg1->data[offset] + arg2->data[offset];
+    		*((int*)dest->data + offset) = *((int*)arg1->data + offset) + *((int*)arg2->data + offset);
     	}	
     }
 }
 void addition_mat_float(struct matrix *dest, struct matrix *arg1, struct matrix *arg2){
-	num_rows = arg1->m; 
-    num_columns = arg1->n;
+    int num_rows = arg1->m; 
+    int num_columns = arg1->n;
     int offset = 0;
     for(int i = 0; i < num_rows; i++){
     	for(int j = 0; j < num_columns; j++){
     		offset = i * num_columns + j;
-    		dest->data[offset] = arg1->data[offset] + arg2->data[offset];
+		*((float*)dest->data + offset) = *((float*)arg1->data + offset) + *((float*)arg2->data + offset);
     	}
     }	
 }
 void addition_vec_int(struct vector *dest, struct vector *arg1, struct vector *arg2){
-    for(int i = 0; i < num_rows; i++){
-    	dest->data[i] = arg1->data[i] + arg2->data[i];
+    for(int i = 0; i < arg1->n; i++){
+    	((int*)(dest->data))[i] = ((int*)(arg1->data))[i]  + ((int*)(arg2->data))[i];
     }
 }
 void addition_vec_float(struct vector *dest, struct vector *arg1, struct vector *arg2){
-    for(int i = 0; i < num_rows; i++){
-    	dest->data[i] = arg1->data[i] + arg2->data[i];
+    for(int i = 0; i < arg1->n; i++){
+    	((float*)(dest->data))[i] = ((float*)(arg1->data))[i]  + ((float*)(arg2->data))[i];
     }
 }
 
 void scalar_multiplication_mat_int(struct matrix *dest, int arg1, struct matrix *arg2){
-	num_rows = arg2->m; 
-    num_columns = arg2->n;
+    int num_rows = arg2->m; 
+    int num_columns = arg2->n;
     int offset = 0;
     for(int i = 0; i < num_rows; i++){
     	for(int j = 0; j < num_columns; j++){
     		offset = i * num_columns + j;
-    		dest->data[offset] = arg2->data[offset] * arg1;
+    		((int*)(dest->data))[offset] = ((int*)(arg2->data))[offset] * arg1;
     	}
     }
 }
 void scalar_multiplication_mat_float(struct matrix *dest, int arg1, struct matrix *arg2){
-	num_rows = arg2->m; 
-    num_columns = arg2->n;
+    int num_rows = arg2->m; 
+    int num_columns = arg2->n;
     int offset = 0;
     for(int i = 0; i < num_rows; i++){
     	for(int j = 0; j < num_columns; j++){
     		offset = i * num_columns + j;
-    		dest->data[offset] = arg2->data[offset] * arg1;
+		((float*)(dest->data))[offset] = ((float*)(arg2->data))[offset] * arg1;
+
     	}
     }
 }
 void scalar_multiplication_vec_int(struct vector *dest, int arg1, struct vector *arg2){
-    for(int i = 0; i < num_rows; i++){
-    	dest->data[i] = arg2->data[i] * arg1;
+    for(int i = 0; i < arg2->n; i++){
+    	((int*)(dest->data))[i] = ((int*)(arg2->data))[i] * arg1;
     }
 }
 void scalar_multiplication_vec_float(struct vector *dest, int arg1, struct vector *arg2){
-    for(int i = 0; i < num_rows; i++){
-    	dest->data[i] = arg2->data[i] * arg1;
-    }
-}
+    for(int i = 0; i < arg2->n; i++){
+    	((float*)(dest->data))[i] = ((float*)(arg2->data))[i] * arg1;
 
+    }
+    
+}
+//seems like mat mul need to rewrite
+/*
 void mat_product_int(struct matrix *dest, struct matrix *arg1, struct matrix *arg2){
-	num_rows = arg1->m; 
-    num_columns = arg1->n;
+    int num_rows = arg1->m; 
+    int num_columns = arg1->n;
     int offset = 0;
     for(int i = 0; i < num_rows; i++){
     	for(int j = 0; j < num_columns; j++){
     		offset = i * num_columns + j;
-    		dest->data[offset] = arg1->data[offset] * arg2->data[offset];
-    	}
+    		((int*)(dest->data))[i] = ((int*)(arg2->data))[i] * arg1;
+
+	}
     }
 }
+
 void mat_product_float(struct matrix *dest, struct matrix *arg1, struct matrix *arg2){
 	num_rows = arg1->m; 
     num_columns = arg1->n;
@@ -284,6 +290,7 @@ void mat_product_float(struct matrix *dest, struct matrix *arg1, struct matrix *
     	}
     }
 }
+*/
 
 void mat_inv_int(struct matrix *dest, struct matrix *src){
 	if(src->m != src->n){

0a8a93a6bd9544adf558b61301c1da69b408d748
madelinezec <mez2113@columbia.edu>
2018-11-27 14:47:37 -0500

    update io c++ with fixed methods from io c file

--- stdlib/io_lib.cpp
+++ stdlib/io_lib.cpp
@@ -5,10 +5,7 @@
 
 #include "matrix_lib.hpp"
 
-#define BUFSIZ 256
 /* read one integer from stdin */
-
-struct matrix_int;
 
 int64_t get_int(){
 /*    int64_t ret;
@@ -16,34 +13,34 @@ int64_t get_int(){
     return ret;
     */
   char buffer[BUFSIZ];
-	read(0, buffer, BUFSIZ);
+  read(0, buffer, BUFSIZ);
 
     int sign = 1;
     size_t offset = 0;
-    //	source for converting string to integer: 
-	//  https://www.programmingsimplified.com/c/source-code/c-program-convert-string-to-integer-without-using-atoi-function
-	if (buffer[0] == '-') {  // Handle negative integers
-    	sign = -1;
-  	}
+    //  source for converting string to integer: 
+  //  https://www.programmingsimplified.com/c/source-code/c-program-convert-string-to-integer-without-using-atoi-function
+  if (buffer[0] == '-') {  // Handle negative integers
+      sign = -1;
+    }
  
-  	if (sign == -1) {  // Set starting position to convert
-    	offset = 1;
-  	}
-  	else {
-    	offset = 0;
-  	}
+    if (sign == -1) {  // Set starting position to convert
+      offset = 1;
+    }
+    else {
+      offset = 0;
+    }
  
-  	int n = 0;
+    int n = 0;
  
-  	for (int c = offset; buffer[c] != '\0'; c++) {
-    	n = n * 10 + buffer[c] - '0';
-  	}
+    for (int c = offset; buffer[c] != '\0'; c++) {
+      n = n * 10 + buffer[c] - '0';
+    }
  
-  	if (sign == -1) {
-    	n = -n;
-  	}
+    if (sign == -1) {
+      n = -n;
+    }
  
-  	return n;
+    return n;
     
 
 }
@@ -54,7 +51,7 @@ void put_int(int x){
     
     char str[BUFSIZ];
 
-	int i = 0; 
+  int i = 0; 
     /*source for while loop: https://bit.ly/2TEkpfb */
     while (x) 
     { 
@@ -82,32 +79,32 @@ float get_float(){
    /* scanf("%f", &ret);
     return ret;
     */
-	char buffer [BUFSIZ];
-	read(0, buffer, strlen(buffer));
-	
-	//source for converting string to float:
-	//https://stackoverflow.com/q/4392665/6637004
-	const char* s = buffer;
-	float rez = 0, fact = 1;
-  	if (*s== '-'){
-    	s++;
-    	fact = -1;
-  	};
+  char buffer [BUFSIZ];
+  read(0, buffer, strlen(buffer));
+  
+  //source for converting string to float:
+  //https://stackoverflow.com/q/4392665/6637004
+  const char* s = buffer;
+  float rez = 0, fact = 1;
+    if (*s== '-'){
+      s++;
+      fact = -1;
+    };
 
-  	for(int point_seen = 0; *s; s++){
-    	if (*s == '.'){
-      		point_seen = 1; 
-      		continue;
-    	}
-    	int d = *s - '0';
-    	if (d >= 0 && d <= 9){
-      		if (point_seen){
-      			fact /= 10.0f;
-      		} 
-      		rez = rez * 10.0f + (float)d;
-  		}
+    for(int point_seen = 0; *s; s++){
+      if (*s == '.'){
+          point_seen = 1; 
+          continue;
+      }
+      int d = *s - '0';
+      if (d >= 0 && d <= 9){
+          if (point_seen){
+            fact /= 10.0f;
+          } 
+          rez = rez * 10.0f + (float)d;
+      }
     }
-  	return rez * fact;
+    return rez * fact;
     
     
 
@@ -117,31 +114,31 @@ float get_float(){
 void put_float(float x){
    /* printf("%f", x);*/
     
-	double integral;
+  double integral;
   float fractional;
   fractional = modf(x, &integral);
 
-	put_int(integral);
-	write(1, ".", strlen("."));
-	put_int(fractional);
+  put_int(integral);
+  write(1, ".", strlen("."));
+  put_int(fractional);
 }
 
 /* print a matrix */
 void put_mat(struct matrix_int* m){
-	int rows = m->m;
-	int columns = m->n;
-	int offset;
-	char new_line [] = "\n";
-	char white_space [] = " ";
+  int rows = m->m;
+  int columns = m->n;
+  int offset;
+  char new_line [] = "\n";
+  char white_space [] = " ";
 
-	for(int i = 0; i < rows; i++){
-		for(int j = 0; j < columns; j++){
-			offset = i * columns + j;
-			put_int(*m->data[offset]);
-			write(1, white_space, strlen(white_space));
-		}
-		write(1, new_line, strlen(new_line));
-	}
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < columns; j++){
+      offset = i * columns + j;
+      put_int(*m->data[offset]);
+      write(1, white_space, strlen(white_space));
+    }
+    write(1, new_line, strlen(new_line));
+  }
 }
 
 void put_mat(struct matrix_float* m){
@@ -163,12 +160,12 @@ void put_mat(struct matrix_float* m){
 
 /* print a vector */
 void put_vec(struct vector_int* v){
-	int length = v->n;
+  int length = v->n;
   char white_space [] = " ";
-	for(int j = 0; j < length; j++){
-		put_int(v->data[j]);
-		write(1, white_space, strlen(white_space));
-	}
+  for(int j = 0; j < length; j++){
+    put_int(v->data[j]);
+    write(1, white_space, strlen(white_space));
+  }
 }
 
 void put_vec(struct vector_float* v){

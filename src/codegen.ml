05e2409a561b21924c56dbd44f16922aa25995da
ZhuohaoLi <34391722+ZhuohaoLi@users.noreply.github.com>
2018-12-18 23:59:13 -0500

    fix while loop

--- src/codegen.ml
+++ src/codegen.ml
@@ -321,24 +321,19 @@ let rec codegen_stmt builder st = function
         ignore @@ L.build_cond_br cond_val then_bb else_bb (L.builder_at_end context start_bb);
         L.position_at_end merge_bb builder
     | While (e, s) ->
+        let cond = codegen_expr builder st e in
+        let zero = L.const_int bool_t 0 in
+        let cond_val = L.build_icmp L.Icmp.Ne cond zero "whilecond" builder in
         let start_bb = L.insertion_block builder in
         let func = L.block_parent start_bb in
-        let cond_bb = L.append_block context "cond_block" func in
-        let loop_bb = L.append_block context "loop_body" func in
-        let loop_builder = L.builder_at_end context loop_bb in
-        codegen_stmt loop_builder st s;
-        let end_bb = L.append_block context "end_block" func in
-       
-        let cond_builder = L.builder_at_end context cond_bb in
-        let cond = codegen_expr cond_builder st e in
-        let zero = L.const_int bool_t 0 in
-
-        let cond_val_builder = L.builder_at_end context cond_bb in 
-        let cond_val = L.build_icmp L.Icmp.Ne cond zero "ifcond" cond_val_builder in
-        ignore @@ L.build_cond_br cond_val loop_bb end_bb (L.builder_at_end context cond_bb);
-        ignore @@ L.build_br cond_bb builder;
-        ignore @@ L.build_br cond_bb (L.builder_at_end context loop_bb);
-        L.position_at_end end_bb builder
+        let body_bb = L.append_block context "loopbody" func in
+        let merge_bb = L.append_block context "endwhile" func in
+        let start_builder = L.builder_at_end context start_bb in
+        let body_builder = L.builder_at_end context body_bb in 
+        let _ = L.build_cond_br cond_val body_bb merge_bb start_builder in
+        codegen_stmt body_builder st body;
+        ignore @@ L.build_br start_bb body_builder;
+        L.position_at_end merge_bb builder
     | _ -> raise CodegenBug
 
 
